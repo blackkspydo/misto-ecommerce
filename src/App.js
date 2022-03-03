604d17f257c28134c22b52b567df9018b48b0495
blackkspydo <Upadhyaya13kishor@gmail.com>
2022-03-03 21:51:07 +0545

    added sales offer section

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Hero from "./components/Layout/Hero/Hero";
 import Women from "./components/Layout/ProductSection/Women";
 import Men from "./components/Layout/ProductSection/Men";
 import Accessories from "./components/Layout/ProductSection/Accessories";
+import SaleBanner from "./components/Layout/SaleBanner/SaleBanner";
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Hero />
+			<div className="product-section">
 			<Women />
 			<Men />
 			<Accessories />
+			</div>
+			<SaleBanner />
 		</div>
 	);
 }
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
+import { BsCartCheckFill } from "react-icons/bs";
+import { motion } from "framer-motion";
 // import Input from "../UI/Input";
 import styles from "./CartButton.module.css";
+
 const CartButton = ({ quantity, onClick }) => {
+	const [added, setadded] = useState(false);
+	const addToCartHandler = () => {
+		setadded(!added);
+		onClick();
+	};
 	return (
 		<div className={styles.cartContainer}>
-            {/* <input type="number" min={0} max={100} /> */}
-            <button
-                className={styles.cartButton}
-            >
-                <span className={styles.cartButton__icon}>
-                    <MdOutlineAddShoppingCart />
-                </span>
-            </button>
-        </div>
+			{/* <input type="number" min={0} max={100} /> */}
+			<button onClick={addToCartHandler} className={styles.cartButton}>
+				{!added ? (
+					<motion.span className={styles.cartButton__icon}>
+						<MdOutlineAddShoppingCart />
+					</motion.span>
+				) : (
+					<motion.span className={styles.cartButton__icon}>
+						<BsCartCheckFill />
+					</motion.span>
+				)}
+			</button>
+		</div>
 	);
 };
 
